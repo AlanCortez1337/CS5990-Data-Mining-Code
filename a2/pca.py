6d3f6a45fa5968b60cfe53814a50c3bd04e8eced
Alan <cortez.alan060@gmail.com>
2025-03-04 23:37:28 -0800

    finish a2

--- a2/pca.py
+++ a2/pca.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Alan Cortez
+# FILENAME: pca.py
+# SPECIFICATION: This program takes the dataset provided and removes one feature at a time to preform PCA and see which feature removed gives us a higher PC1 variance
 # FOR: CS 5990 (Advanced Data Mining) - Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: About 18-24 hours
 # -----------------------------------------------------------*/
 
 #importing some Python libraries
@@ -14,11 +14,11 @@ from sklearn.preprocessing import StandardScaler
 
 #Load the data
 #--> add your Python code here
-df = ?
+df = pd.read_csv('./heart_disease_dataset.csv')
 
-#Create a training matrix without the target variable (Heart Diseas)
+#Create a training matrix 
 #--> add your Python code here
-df_features = ?
+df_features = df.values
 
 # Standardize the data
 scaler = StandardScaler()
@@ -26,13 +26,15 @@ scaled_data = scaler.fit_transform(df_features)
 
 #Get the number of features
 #--> add your Python code here
-num_features = ?
+num_features = len(df_features[0])
 
+pca_data = {}
 # Run PCA for 9 features, removing one feature at each iteration
 for i in range(num_features):
     # Create a new dataset by dropping the i-th feature
     # --> add your Python code here
-    reduced_data = ?
+    target_feature = df.columns[i]
+    reduced_data = df.drop(target_feature, axis=1)
 
     # Run PCA on the reduced dataset
     pca = PCA()
@@ -41,13 +43,21 @@ for i in range(num_features):
     #Store PC1 variance and the feature removed
     #Use pca.explained_variance_ratio_[0] and df_features.columns[i] for that
     # --> add your Python code here
+    pca_data[df.columns[i]] = pca.explained_variance_ratio_[0]
+
 
 # Find the maximum PC1 variance
 # --> add your Python code here
+best_removed_feature = ""   
+best_pc1 = -1
+for feature, pc1 in pca_data.items():
+    if pc1 > best_pc1:
+        best_pc1 = pc1
+        best_removed_feature = feature
 
 #Print results
 #Use the format: Highest PC1 variance found: ? when removing ?
-
+print(f"Highest PC1 variance found: {best_pc1} when removing {best_removed_feature}")
 
 
 
